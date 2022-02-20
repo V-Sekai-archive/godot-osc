710095f8ba5148063be2604515a481dfd13cc2ed
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-02-20 03:23:41 -0800

    Simplify.

--- config.py
+++ config.py
@@ -4,16 +4,3 @@ def can_build(env, platform):
 
 def configure(env):
     pass
-
-
-def get_doc_classes():
-    return [
-        "Speech",
-        "SpeechDecoder",
-        "SpeechProcessor",
-    ]
-
-
-def get_doc_path():
-    return "doc_classes"
-
